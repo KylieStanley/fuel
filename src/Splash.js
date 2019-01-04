d3a19b25d4a140049475ec3be86cb70b188be164
Kylie Stanley <knstanley11@gmail.com>
2019-01-03 23:28:14 -0700

    Add files and start Splash page

---
+++ src/Splash.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles/main.scss';
+import { Link } from 'react-router-dom'
+
+
+const Splash = () => {
+	return (
+		<div className="splash">
+			<div className="ovrlay"></div>
+			<div className="splash-green"></div>
+			<div className="splash-text">
+				<h1>fuel</h1>
+				<h2>Recipes and Nutrition to support your training</h2>
+				<Link to='/main/'>
+					<button className="splash-btn">Find Your Fuel <i className="fas fa-caret-right"></i></button>
+				</Link>
+			</div>
+		</div>	
+	)	
+}
+
+export default Splash
