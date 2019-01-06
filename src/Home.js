ebb540aa9bb521061235108c303154eff3524f4f
Kylie Stanley <knstanley11@gmail.com>
2019-01-06 11:22:49 -0700

    Style menu active class

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Search from './Search'
+import Menu	 from './Menu'
 import CardContainer from './containers/CardContainer'
 
 const Home = () => {
 	return (
 		<div className="main-container">
+		<Menu />
 			<h1 className="header">fuel<span>favorites</span></h1>
 			<CardContainer />
 		</div>
